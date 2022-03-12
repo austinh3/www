06e5a9fcf1ed2979057f77f8a3144cfd3105cd0b
Bill <5141718+wcorrigan@users.noreply.github.com>
2022-03-12 09:33:49 -0800

    form created and url added

--- assets/js/boatparty.js
+++ assets/js/boatparty.js
@@ -4,7 +4,7 @@ $("#boatparty-form").submit(function (e) {
   e.preventDefault();
   //AJAX request
   $.ajax({
-    url: "https://docs.google.com/forms/d/e/1FAIpQLSe4O3DJpe61FXMlnzmV48-jU8XMLS8kUhcvd8SF6DIoY6QpDA/formResponse", //The public Google Form url, but replace /view with /formResponse
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSdNM0xxa05T_MsR29tw82zPbnWjheLfIn06SsZ54P8W9mno0Q/formResponse", //The public Google Form url, but replace /view with /formResponse
     data: $("#boatparty-form").serialize(), //Nifty jquery function that gets all the input data
     type: "POST", //tells ajax to post the data to the url
     dataType: "json", //the standard data type for most ajax requests
